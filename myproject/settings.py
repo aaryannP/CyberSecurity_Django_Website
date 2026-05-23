d70e3b5256ae629eaa626f0b9ecfa4967d4248b2
aaryannP <aryanparmar5116@gmail.com>
2026-05-23 08:39:55 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -114,7 +114,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
