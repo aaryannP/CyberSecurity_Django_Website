c5e399ef2dc6270b3acb43fa91fef2983110c4d0
aaryannP <aryanparmar5116@gmail.com>
2026-05-23 11:39:31 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,8 +118,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = 'aryanparmar5116@gmail.com'
 EMAIL_HOST_PASSWORD = 'wacm evdg qjea xvix'
